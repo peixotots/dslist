c3fe5a9752a0a6db9957cb06cb8f9a60af3af00c
Tainá Peixoto <tsouzapeixoto@gmail.com>
2025-01-26 21:15:22 -0300

    feat: Implementa endpoint que reposiciona os games na lista
    Usado lista em memória para realizar o reposicionamento
    
    Atualiza GameListRepository com query para modificar a posição dos jogos no banco de dados
    
    Método ‘move’ em ‘GameListService’ para atualiza a posição dos jogos na lista
    
    sourceIndex e destinationIndex representam a posição de origem e destino, respectivamente
    
    obj representa o objeto que foi removido e, em seguida, inserido novamente na lista
    
    ReplacementDTO encapsula os índices de origem e destino

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destinationIndex;
+
+    private ReplacementDTO(){
+
+    }
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestinationIndex() {
+        return destinationIndex;
+    }
+
+    public void setDestinationIndex(Integer destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+
+}
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +17,39 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 
         return dto;
+    }
 
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        // Atualização em memória da posição
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        // A origem e destino (primeira e última posições) não são alterados
+        // Determina os valores mínimo e máximo entre os índices sourceIndex e destinationIndex
+        // Garante que min sempre terá o menor valor e max sempre terá o maior valor entre os dois índices fornecidos
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        // Atualiza a posição dos elementos no intervalo [min, max] na lista no banco de dados
+        // Parâmetros do metodo updateBelongingPosition:
+        // listId: O ID da lista que está sendo atualizada
+        // list.get(i).getId(): O ID do jogo na posição i da lista
+        // i: A nova posição do jogo na lista
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 
 }
